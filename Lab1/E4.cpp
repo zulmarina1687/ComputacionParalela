84c8636b2d4827426dcea6c0bd50bd584a4948a7
zulmarina1687 <luzmarinapilco@gmail.com>
2020-09-24 06:53:16 -0500

    ejercicio4

--- Lab1/E4.cpp
+++ Lab1/E4.cpp
@@ -8,14 +8,28 @@ int min(int a, int b){
 int main(){
     for (int size = 100; size < 800; size += 100){
         cout<<"Size "<<size<<":"<<"\n";
-        int A[size][size], B[size][size], C[size][size];
+        int A[size][size], B[size][size], C[size][size], CB[size][size];
         for (int i = 0;i < size;i++){
             for (int  j= 0; j < size; j++){
                 A[i][j] = rand() % 10000 + 1;
                 B[i][j] = rand() % 10000 + 1;
                 C[i][j] = 0;
+                CB[i][j] = 0;
             }
         }
+        auto t0 = chrono::system_clock::now();
+        for(int i=0; i<size; ++i){
+            for(int j=0; j<size; ++j){
+                for(int z=0; z<size; ++z){
+                    C[i][j] += A[i][z] * B[z][j];
+                }
+            }
+        }     
+        auto t1 = chrono::system_clock::now();
+        auto time = chrono::duration_cast<chrono::milliseconds>(t1-t0).count();
+        cout<<"Clásica: "<<time/1000.0<<"\n";
+
+
         auto t2 = chrono::system_clock::now();
         int s = size / 25;
         int temp = 0;
@@ -27,7 +41,7 @@ int main(){
                         for(int k = kk; k<((kk+s)>size?size:(kk+s)); k++){
                             temp += A[i][k]*B[k][j];
                         }
-                        C[i][j] += temp;
+                        CB[i][j] += temp;
                     }
                 }
             }
