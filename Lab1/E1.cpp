18b7ebf28b38df81db79015b2c90ab43435eb0a1
zulmarina1687 <luzmarinapilco@gmail.com>
2020-09-23 22:11:28 -0500

    ejercicio1

---
+++ Lab1/E1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+void inicializar(double **&A, double *&x, double *&y, int MAX){
+    A = new double*[MAX];
+    x = new double[MAX];
+    y = new double[MAX];
+    for(int i=0;i<MAX;++i){
+        A[i]=new double[MAX];
+    }
+}
+void bucle1(int MAX){
+    double **A,*x,*y;
+    inicializar(A,x,y,MAX);
+    auto t0 = chrono::system_clock::now();
+    for(int i=0;i<MAX;i++){
+        for(int j=0;j<MAX;j++){
+            y[i] += A[i][j]*x[j];
+        }
+    }
+    auto t1 = chrono::system_clock::now();
+    auto time = chrono::duration_cast<chrono::milliseconds>(t1-t0).count();
+    cout<<"Bucle 1: "<<time/1000.0<<"\n";
+}
+
+void bucle2(int MAX){
+    double **A,*x,*y;
+    inicializar(A,x,y,MAX);
+    auto t0 = chrono::system_clock::now();
+    for(int j=0;j<MAX;j++){
+        for(int i=0;i<MAX;i++){
+            y[i] += A[i][j]*x[j];
+        }
+    }
+    auto t1 = chrono::system_clock::now();
+    auto time = chrono::duration_cast<chrono::milliseconds>(t1-t0).count();
+    cout<<"Bucle 2: "<<time/1000.0<<"\n";
+}
+int main(int argc, char const** argv){
+    int MAX = 10;
+    for(int i=0;i<5;++i){
+        cout<<"Test "<<i+1<<" con MAX = "<<MAX<<":"<<endl;
+        bucle1(MAX);
+        bucle2(MAX);
+        MAX *=10;
+    }
+    return 0;
+}
